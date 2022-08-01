0fc3f6a4c5a5f1818ca189c14c9e7cb64a0937c3
Soheil SALMANI <soheil.salmani@ledger.fr>
2022-08-01 17:25:10 +0200

    initial commit

--- tasks/app/main.py
+++ tasks/app/main.py
@@ -1,4 +1,8 @@
+import pkgutil
+
 from utils.greetings import say_hello
 
 if __name__ == "__main__":
+    config = pkgutil.get_data("app", "config.json").decode("utf-8")
+    print(config)
     say_hello()
